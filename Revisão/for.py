6dab88e82661543032dd0a5835b7c632c88868b0
João Pedro Paiva <jppaiva1706@gmail.com>
2025-04-04 17:31:20 -0300

    for

---
+++ Revisão/for.py
@@ -0,0 +1,29 @@
+text = 'Python'
+
+for l in text:
+    print(l)
+
+print(' ')
+
+for i in range(len(text)):
+    print(i, text[i])
+
+print(' ')
+
+for i, p in enumerate(text):
+    print(i, p)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
