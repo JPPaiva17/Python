56cf01cccd5462eee6faa611c63da45c33755067
João Pedro Paiva <jppaiva1706@gmail.com>
2025-04-04 13:54:04 -0300

    Tipos primitivos em python

---
+++ Revisão/tiposPrimitivos.py
@@ -0,0 +1,24 @@
+# Os tipos primitivos de python sao os seguintes:
+
+# Int -> Numeros inteiros 1,2,3,4...
+
+print(777)
+print(type(777))
+
+# Str -> Strings (texto), tudo que esta entre '' ou ""
+
+
+print('Joao')
+print(type('Joao'), type('777'))
+
+# Float -> Numeros decimais, separando o inteiro do decimal por .
+
+
+print(777.333)
+print(type(777.333))
+
+# Boolean -> Verdadeiro ou Falso
+
+print(True)
+print(False)
+print(type(True), type(False))
