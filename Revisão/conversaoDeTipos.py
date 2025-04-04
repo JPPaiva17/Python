427c684d84251e0186e4ad58f6aba5c6bd0a6b06
João Pedro Paiva <jppaiva1706@gmail.com>
2025-04-04 13:59:04 -0300

    Conversão de tipos

---
+++ Revisão/conversaoDeTipos.py
@@ -0,0 +1,23 @@
+# Nós podemos converter tipos entre si
+# Conversão de ti´ps, coerção, typecasting, type convertion, coercion
+# Tipos imutáveis e Primitivos: str, int, float e bool
+
+# Convertendo para Int
+print('1' + '1')
+print(type('1' + '1'))
+print(int('1' + '1'))
+print(type(int('1' + '1')))
+
+# Convertendo para float
+print(float('7' + '7.1'))
+print(type(float('7' + '7.1')))
+
+# Convertendo para bool
+print(bool(0))
+print(bool(''))
+print(bool(1))
+print(bool(1231245))
+print(bool('dnviudfnbuinfbmd'))
+
+# Convertendo para str
+print(str(10)+str(30))
