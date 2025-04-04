3626c01cc8a14492dd1062c9d1b7985ff4270298
João Pedro Paiva <jppaiva1706@gmail.com>
2025-04-04 13:34:12 -0300

    revisao print

---
+++ Revisão/print.py
@@ -0,0 +1,11 @@
+# A função print é utilizada para exibir coisas na tela
+# Ela é multiargumentos
+# A cada print existe uma quebra de linha
+# é possível mudar o separador de um print com varios elementos utilizando sep = 'oqVcQuiser'
+# Para printar quebra de linha só utilizar \n
+# No argumento end a gente pode dizer o que ele ira realizar apos o print
+
+print('Hello World!')
+print('Hello', 'World!', sep = '_')
+
+
