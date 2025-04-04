f9a7a49d5f3adb52f8b6835f57a4dc66b088df2f
João Pedro Paiva <jppaiva1706@gmail.com>
2025-04-04 13:47:52 -0300

    Algumas coisas sobre strings

---
+++ Revisão/strings.py
@@ -0,0 +1,19 @@
+# String e um tipo basico do python
+
+palavra = "Paralelepipedo"
+print(type(palavra))
+
+# Strings podem ser somadas e multiplicadas
+
+print(2*palavra)
+
+print(palavra + " e tijolo")
+
+# Tambem podemos acessar certos caracteres da palavra atraves do index
+# Index [inicio:fim(exclusivo):passo]
+print(palavra[0])
+print(palavra[0:2])
+print(palavra[0::2])
+print(palavra[-1::-1]) #Inverti a string comecei pelo indice -1(Ultimo caractere) com um passo de -1
+
+
