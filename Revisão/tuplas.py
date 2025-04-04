445746047c27bfce8997be550bee0473484a33a2
João Pedro Paiva <jppaiva1706@gmail.com>
2025-04-04 18:49:28 -0300

    Tuplas

---
+++ Revisão/tuplas.py
@@ -0,0 +1,12 @@
+# São como listas mas são imutáveis
+
+tupla = ('joao', 'pedro', 'paiva')
+tupla2 = 'joao', 'pedro', 'paiva'
+tupla3 = ()
+tupla4 = tuple()
+
+
+# Da para fazer o contrario tb
+
+nomes = ['joao', 'pedro', 'paiva']
+tupla5 = tuple(nomes)
