1c3d76fba277e66e0a099d223a221079563849c5
João Pedro Paiva <jppaiva1706@gmail.com>
2025-04-05 20:45:12 -0300

    Mais alguns metodos de dicionario

--- Revisão/dicionarios.py
+++ Revisão/dicionarios.py
@@ -9,6 +9,26 @@ pessoa = {
 
 pessoa['CorDosOlhos'] = 'Marrom'
 
+# Metodo Get
+
 print(pessoa.get('sobrenome'))
 
 print(pessoa.get('pudim', None))
+
+# len(dicionario) -> qtd de chaves
+
+print(len(pessoa))
+
+# dicionario.keys() -> retorna as chaves do dicionario
+
+print(list(pessoa.keys()))
+
+
+# dicionario.values() -> retorna os valores do dicionario
+
+print(list(pessoa.values()))
+
+
+# dicionario.items() -> retorna o par chave-valor do dicionario
+
+print(list(pessoa.items()))
